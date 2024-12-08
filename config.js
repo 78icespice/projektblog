28f0bc7f8cb1822d0c7a6098d9140bafa076cb15
liwi78 <lino.wiedmer@bbz-cfp.ch>
2024-12-08 13:35:42 +0000

    finish

--- config.js
+++ config.js
@@ -6,11 +6,17 @@ import cookieParser from "cookie-parser";
 import multer from "multer";
 const upload = multer({ dest: "public/uploads/" });
 import sessions from "express-session";
+import bbz307 from "bbz307";
 
 export function createApp(dbconfig) {
   const app = express();
 
   const pool = new Pool(dbconfig);
+  const login = new bbz307.Login(
+    "users",
+    ["benutzername", "passwort" "profilbild"],
+    pool
+  );
 
   app.engine("handlebars", engine());
   app.set("view engine", "handlebars");
@@ -28,6 +34,57 @@ export function createApp(dbconfig) {
       resave: false,
     })
   );
+
+  app.get("/register", (req, res) => {
+    res.render("register");
+  });
+
+  app.post("/register", upload.none(), async (req, res) => {
+    const user = await login.registerUser(req);
+    if (user) {
+      res.redirect("/login");
+      return;
+    } else {
+      res.redirect("/register");
+      return;
+    }
+  });
+
+  app.get("/login", (req, res) => {
+    res.render("login");
+  });
+
+  app.post("/login", upload.none(), async (req, res) => {
+    const user = await login.loginUser(req);
+    if (!user) {
+      res.redirect("/login");
+      return;
+    } else {
+      res.redirect("/intern");
+      return;
+    }
+  });
+
+  app.get("/event_formular", function (req, res) {
+    res.render("event_formular");
+  });
+
+  app.post("/event", upload.none(), async function (req, res) {
+    await pool.query(
+      "INSERT INTO events (event_name, description) VALUES ($1, $2)",
+      [req.body.event_name, req.body.description]
+    );
+    res.redirect("/");
+  });
+
+  app.post("/event", upload.none(), async function (req, res) {
+    const user = await login.loggedInUser(req);
+    await pool.query(
+      "INSERT INTO event (event_name, description) VALUES ($1, $2, $3)",
+      [req.body.event_name, req.body.description, user.id]
+    );
+    res.redirect("/");
+  });
 
   app.locals.pool = pool;
 
--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-import { createApp } from "./config.js";
+import { createApp, upload } from "./config.js";
 
 const app = createApp({
-  user: "autumn_star_7622",
-  host: "168.119.168.41",
-  database: "demo",
-  password: "uaioysdfjoysfdf",
-  port: 18324,
+  user: "maelin",
+  host: "cloud.bbz",
+  database: "maelin",
+  password: "fMQpY3ScBPm7GE8RFNhV",
+  port: 30211,
 });
 
 /* Startseite */
 app.get("/", async function (req, res) {
   res.render("start", {});
+});
+
+app.get("/new_post", async function (req, res) {
+  res.render("new_post", {});
+});
+
+app.get("/", async function (req, res) {
+  const events = await app.locals.pool.query("select * form events");
+  res.render("start", { events: events.rows });
+});
+
+app.post("/create_post", upload.single("image"), async function (req, res) {
+  const result = await app.locals.pool.query(
+    "INSERT INTO todos (text, dateiname) VALUES ($1, $2)",
+    [req.body.text, req.file.filename]
+  );
+  console.log(result);
+  res.redirect("/");
 });
 
 app.get("/impressum", async function (req, res) {
@@ -20,4 +38,8 @@ app.get("/impressum", async function (req, res) {
 /* Wichtig! Diese Zeilen müssen immer am Schluss der Website stehen! */
 app.listen(3010, () => {
   console.log(`Example app listening at http://localhost:3010`);
+});
+
+app.get("/news", async function (req, res) {
+  res.render("news", {});
 });
